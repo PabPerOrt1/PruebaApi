e7f299d8fed40a3c2f933c83db0ea78941c17186
pabperort1 <pabperort1@alum.us.es>
2023-03-25 02:40:17 +0100

    Faltan algunas cosas

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ import express from "express";
 import cors from "cors";
 
 import { loadBackend } from "./backend/index.js";
-import { handler } from "./frontend/build/handler.js";
+
 
 var app = express();
 
 app.use(cors());
 
-var port = process.env.PORT || 12345;
+app.use("/",express.static("./public"));
+var port = process.env.PORT || 2222;
 
 app.use(express.json());
 
 loadBackend(app);
-app.use(handler);
+
 
 app.listen(port,()=>{
     console.log(`Server ready in port ${port}`);
